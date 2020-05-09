6c9b6d37bdb5202e4c074221b4017f39e9da416e
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-05-09 18:19:52 +0700

    test

---
+++ session1.php
@@ -0,0 +1,17 @@
+<?php
+$x = 10;//$x biến trong php
+
+/*
+ * kiểu dữ liệu
+ * number
+ * string
+ * array
+ * object
+ * boolean
+ */
+
+$y = $x+10; //y=20
+echo "y = ".$y;
+$x = "hello";
+$y = $x+10; //y = "hello10"
+echo "y = ".$y;
